b03e87e3aaed0a46931b536c9cad0911e30a9ede
jazyk3895 <andysejati@gmail.com>
2021-04-25 17:02:21 +0700

    Added Enemy 1, Enemy 2 prefabs

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 1;
+    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] private SpriteRenderer _healthBar;
+    [SerializeField] private SpriteRenderer _healthFill;
+
+    private int _currentHealth;
+
+    private void OnEnable()
+    {
+        _currentHealth = _maxHealth;
+        _healthFill.size = _healthBar.size;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
